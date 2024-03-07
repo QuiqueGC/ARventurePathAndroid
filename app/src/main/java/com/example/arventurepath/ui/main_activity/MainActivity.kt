469f4a4f5841dce68fffa7509ece7781ac4bc2ec
Quique <124347538+eguevarac@users.noreply.github.com>
2024-03-07 21:49:25 +0100

    Añadidos los fragments con sus layouts

--- app/src/main/java/com/example/arventurepath/ui/main_activity/MainActivity.kt
+++ app/src/main/java/com/example/arventurepath/ui/main_activity/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.arventurepath
+package com.example.arventurepath.ui.main_activity
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.arventurepath.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/com/example/arventurepath/ui/score_fragment/ScoreFragment.kt
@@ -0,0 +1,20 @@
+package com.example.arventurepath.ui.score_fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.arventurepath.R
+
+class ScoreFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_score, container, false)
+    }
+
+}
