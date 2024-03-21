1fc1ee84d32d17ccbcce3497fd20cfc71287bf41
Leo Ferraz <lferrazm2122@politecnics.barcelona>
2024-03-21 20:02:48 +0100

    Implementando contador de pasos en InGameFragment.kt y permiso en AndroidManifest.xml

--- app/src/main/java/com/example/arventurepath/ui/in_game_fragment/InGameFragment.kt
+++ app/src/main/java/com/example/arventurepath/ui/in_game_fragment/InGameFragment.kt
@@ -1,15 +1,20 @@
 package com.example.arventurepath.ui.in_game_fragment
 
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.navArgs
-import com.example.arventurepath.data.StepCounter
 import com.example.arventurepath.data.models.Stop
 import com.example.arventurepath.databinding.FragmentInGameBinding
 import com.example.arventurepath.ui.detail_arventure_fragment.DetailArventureFragmentArgs
@@ -22,48 +27,94 @@ import com.google.android.gms.maps.model.MarkerOptions
 import kotlinx.coroutines.launch
 
 
-class InGameFragment : Fragment(), OnMapReadyCallback {
+class InGameFragment : Fragment(), OnMapReadyCallback, SensorEventListener {
 
     private lateinit var binding: FragmentInGameBinding
     private val args: DetailArventureFragmentArgs by navArgs()
-    private var stepCounter: StepCounter? = null
     private var totalSeconds: Int = 0
-    private lateinit var handler: Handler
+    private lateinit var handlerTime: Handler
 
     private val viewModel = InGameViewModel()
 
     private lateinit var map: GoogleMap
 
+    private var sensorManager: SensorManager? = null
+    private var stepSensor: Sensor? = null
+    private var stepCount = 0
+
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-
         binding = FragmentInGameBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        handler = Handler(Looper.getMainLooper())
 
+        //Timer tiempo
+        handlerTime = Handler(Looper.getMainLooper())
         startTimer()
+
+        //Contador de pasos
+        sensorManager = requireActivity().getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        stepSensor = sensorManager?.getDefaultSensor(Sensor.TYPE_STEP_COUNTER)
+        startStepCounter()
 
         viewModel.getArventureDetail(args.idArventure)
         observeViewModel()
         setMap()
     }
 
+    private fun startStepCounter() {
+        stepSensor?.let { sensor ->
+            sensorManager?.registerListener(this, sensor, SensorManager.SENSOR_DELAY_NORMAL)
+        }
+    }
+    override fun onSensorChanged(event: SensorEvent?) {
+        Log.e("ON SENSOR CHANGED" , " antes del IF y del event.let")
+        event?.let {
+            Log.e("ON SENSOR CHANGED" , " antes del IF y DENTRO event.let")
+            if (it.sensor == stepSensor) {
+                stepCount = it.values[0].toInt()
+                Log.e("ON SENSOR CHANGED" , " ENTRÓ EN EL IF$stepCount")
+                updateStepCount()
+            }
+        }    }
+
+    private fun updateStepCount() {
+        Log.e("updateStepCount" , "ANTES DE ENTRAR EN RUNONUITHREAD")
+        requireActivity().runOnUiThread {
+            Log.e("updateStepCount" , "DESPÚES DE ENTRAR EN RUNONUITHREAD")
+            binding.stepsValueText.text = "$stepCount"
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        startStepCounter()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        sensorManager?.unregisterListener(this)
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+        TODO("Not yet implemented")
+    }
     private fun startTimer(){
         // Ejecutar un Runnable cada segundo para contar los segundos
-        handler.post(object : Runnable {
+        handlerTime.post(object : Runnable {
             override fun run() {
                 // Incrementar los segundos
                 totalSeconds++
                 // Actualizar la UI con los segundos transcurridos en formato de horas, minutos y segundos
                 updateUI()
                 // Ejecutar este Runnable nuevamente después de 1 segundo
-                handler.postDelayed(this, 1000)
+                handlerTime.postDelayed(this, 1000)
             }
         })
     }
