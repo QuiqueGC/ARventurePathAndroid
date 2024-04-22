ca89e15c10984a2b8ebae2ea18ba2bcd07c9f0b9
Leo Ferraz <lferrazm2122@politecnics.barcelona>
2024-04-22 18:42:49 +0200

    TutorialFragment.kt

--- app/src/main/java/com/example/arventurepath/ui/tutorial_fragment/TutorialFragment.kt
+++ app/src/main/java/com/example/arventurepath/ui/tutorial_fragment/TutorialFragment.kt
@@ -1,9 +1,12 @@
 package com.example.arventurepath.ui.tutorial_fragment
 
+import android.media.MediaPlayer
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.MediaController
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
@@ -28,6 +31,18 @@ class TutorialFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        //val videoPath = "android.resource://" + requireContext().packageName + "/" + R.raw.tutorial_arventurepath
+        //val videoUri = Uri.parse(videoPath)
+
+        val mediaController = MediaController(requireContext())
+        mediaController.setAnchorView(binding.videoViewTuto)
+        binding.videoViewTuto.setMediaController(mediaController)
+
+        //binding.videoViewTuto.setVideoURI(videoUri)
+
+        binding.videoViewTuto.start()
+
 
         binding.buttonSkip.setOnClickListener{
             findNavController().navigate(
