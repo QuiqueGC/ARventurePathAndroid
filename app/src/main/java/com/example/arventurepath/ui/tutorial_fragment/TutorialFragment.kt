6f43c2cfd323044888904e2068ea3f88f089608d
Quique <enriquegcameselle@gmail.com>
2024-04-22 20:18:47 +0200

    Added tutorial

--- app/src/main/java/com/example/arventurepath/ui/tutorial_fragment/TutorialFragment.kt
+++ app/src/main/java/com/example/arventurepath/ui/tutorial_fragment/TutorialFragment.kt
@@ -1,6 +1,5 @@
 package com.example.arventurepath.ui.tutorial_fragment
 
-import android.media.MediaPlayer
 import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -10,11 +9,8 @@ import android.widget.MediaController
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
-import com.bumptech.glide.Glide
 import com.example.arventurepath.R
 import com.example.arventurepath.databinding.FragmentTutorialBinding
-import com.example.arventurepath.ui.list_arventure_fragment.ListArventureFragmentArgs
-import com.example.arventurepath.ui.login_fragment.LoginFragmentDirections
 
 class TutorialFragment : Fragment() {
 
@@ -32,14 +28,15 @@ class TutorialFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        //val videoPath = "android.resource://" + requireContext().packageName + "/" + R.raw.tutorial_arventurepath
-        //val videoUri = Uri.parse(videoPath)
+        val videoPath =
+            "android.resource://" + requireContext().packageName + "/" + R.raw.tutorial_arventure_path
+        val videoUri = Uri.parse(videoPath)
 
         val mediaController = MediaController(requireContext())
         mediaController.setAnchorView(binding.videoViewTuto)
         binding.videoViewTuto.setMediaController(mediaController)
 
-        //binding.videoViewTuto.setVideoURI(videoUri)
+        binding.videoViewTuto.setVideoURI(videoUri)
 
         binding.videoViewTuto.start()
 
